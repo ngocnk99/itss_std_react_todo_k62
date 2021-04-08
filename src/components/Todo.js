66239535ab0d0281f21bf79e8249d741028580fe
ngocnk99 <ngocnk99@gmail.com>
2021-04-08 10:07:05 +0700

    Create by other

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -33,14 +33,15 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+        <TodoItem
+          key={item.key}
+          item={item}
+        />
       ))}
       <div className="panel-block">
         {items.length} items
       </div>
+      
     </div>
   );
 }
